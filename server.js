8db6c2446940bf85d4ae304677d80453afcffb50
Carlos-Henrique-G <mateus.vieira@ifce.edu.br>
2025-04-15 14:44:21 -0300

    subindo versao
    criando nova versao pw3

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+//app.unsubscribe(express.urlencoded({extended:true}));
+
+
+app.get('/',(req,res)=>{
+    res.send('olá mundo, nov dsadso');
+});
+
+app.post('/',(req,res)=>{
+    res.send('olá mundo, post');
+});
+
+app.use(express.json());
+app.post('/login',(req,res)=>{
+
+    const {user, password} = req.body;
+    
+    if (!user || !password){
+        return res.status(401).json({error:'usuário e senha são obrigatorios!!'})
+    }
+
+    if(user === 'admin' && password === '123'){
+        return res.status(200).json({msg:'usuario logado com sucesso!!'})
+    }else{
+        return res.status(401).json({error:'usuário e senha invalido!!'})
+    }
+
+});
+
+app.post('/user/save',(req,res)=>{
+    const {name, email} = req.body;
+    const user = {name, email};
+    return res.status(200).json(user)
+})
+
+app.listen(port,()=>{
+    console.log(`servidor rodando na porta ${port}`)
+})
